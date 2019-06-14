536be5237067581b9753c423f51b989856612ff0
Sontaya0716 <58160434@go.buu.ac.th>
2019-06-15 01:51:31 +0700

    angular connect api

--- node/server.js
+++ node/server.js
@@ -1,15 +1,37 @@
 // Backend
 const express = require('express');
+const bodyParser = require('body-parser');
+
 const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded());
+
+//Allow client to access cross domain or ip-address
+app.use(function (req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'content-type, x-access-token');
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    next();
+});
 
 app.get('/', (req, res) => {
     res.end("welcome to root path");
-})
+});
 
 app.get('/home', (req, res) => {
     res.end("welcome to home path");
-})
+});
+
+app.post('/api', (req, res) => {
+    const feedback = req.body.feedback;
+    const username = req.body.username;
+
+    //res.end("Received Feedback: " + feedback + ", Username: " + username);
+    res.json({result: "success", username: username, feedback: feedback})
+});
 
 app.listen(3000, () => {
     console.log("Server is running...");
-})
+});
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
+  
   title = 'helloAngular';
+
+  constructor(private http:HttpClient) {
+
+  }
+
+  ngOnInit(): void {
+    let data = {username: "Golf", feedback: "I love it"};
+    this.http.post<any>('http://localhost:3000/api', data).subscribe(result=>{
+      alert(JSON.stringify(result));
+    })
+  }
 }
